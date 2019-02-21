0e8cac31cb8401bd6f65cad98fc94648ebf139cd
Rahul Chandera <rahul.crc@gmail.com>
2019-02-21 17:30:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 exports.handler = function (event, context, callback) {
-
+  
   var AWS = require('aws-sdk');
   const D3Node = require('/opt/node_modules/d3-node') // Because we have used AWS Layers for all dependancies, we have to append Layers directory path when we import any external dependancies. 
   const options = { selector: '#chart', container: '<div id="container"><div id="chart"></div></div>' } // Need to define one container to render charts
@@ -10,24 +10,31 @@ exports.handler = function (event, context, callback) {
 
   // Dummy data
   var data = [
-    { value: 99, label: 'Mon' },
-    { value: 63, label: 'Tues' },
-    { value: 41, label: 'Wed' },
-    { value: 100, label: 'Thur' },
-    { value: 50, label: 'Fri' },
-    { value: 23, label: 'Sat' },
-    { value: 8, label: 'Sun' }
+    { value: 20, label: 'Mon' },
+    { value: 25, label: 'Tues' },
+    { value: 5, label: 'Wed' },
+    { value: 10, label: 'Thur' },
+    { value: 15, label: 'Fri' },
+    { value: 18, label: 'Sat' },
+    { value: 7, label: 'Sun' }
   ];
 
   // Define the dimensions
   var dimensions = {
     gWidth: 600,
-    gHeight: 400,
+    gHeight: 1500,
     gMargin: 40,
     gInnerWidth: 520,
     gInnerHeight: 320,
-    bMargin: 10
+    bMargin: 10,
+    radius: 200
   };
+
+
+  //=====================================================================//
+
+
+  // Bar Chart
 
   // Define the scales
   var xScale = d3.scaleLinear()
@@ -90,17 +97,136 @@ exports.handler = function (event, context, callback) {
     .enter()
     .append('rect')
     .attr("class", 'bar-line')
-    .style("fill", "#E67E22")
     .attr("transform", 'translate(' + dimensions.gMargin + ', ' + dimensions.gMargin + ')')
     .attr("height", function (d, i) { return yScale(d.value); })
     .attr("width", (dimensions.gInnerWidth / data.length) - (dimensions.bMargin * 2))
     .attr("x", function (d, i) { return (dimensions.gInnerWidth / data.length) * i + dimensions.bMargin; })
     .attr("y", function (d, i) { return dimensions.gInnerHeight - yScale(d.value); })
+    .style("fill", "steelblue")
+
+
+  //=====================================================================//
+
+
+  // Pie Chart
+
+  // Add chart title
+  chart.append("g")
+    .attr("transform", "translate(" + (dimensions.gWidth / 2 - 60) + "," + 480 + ")")
+    .append("text")
+    .text("Pie Chart Sample")
+    .attr("class", "title")
+
+  // Add chart group element
+  var g = chart.append("g")
+    .attr("transform", "translate(" + 300 + "," + 700 + ")");
+
+  // Init pie chart with values             
+  var pie = d3.pie()
+    .value(function (d) { return d.value; });
+
+  // Create group element for each data values
+  var arc = g.selectAll(".arc")
+    .data(pie(data))
+    .enter().append("g")
+    .attr("class", "arc");
+
+  // Init arc with inner & outer radius
+  var path = d3.arc()
+    .outerRadius(dimensions.radius - 10)
+    .innerRadius(0);
+
+  // Colors for individual arc             
+  var color = d3.scaleOrdinal(['#4daf4a', '#377eb8', '#ff7f00', '#984ea3', '#e41a1c', '#48C9B0', '#F7DC6F']);
+
+  // Plot color into all arcs          
+  arc.append("path")
+    .attr("d", path)
+    .attr("fill", function (d) { return color(d.value); });
+
+  // Init arc object to plot labels
+  var label = d3.arc()
+    .outerRadius(dimensions.radius)
+    .innerRadius(dimensions.radius - 80);
+
+  // Add text in every arc
+  arc.append("text")
+    .attr("transform", function (d) { return "translate(" + label.centroid(d) + ")"; })
+    .text(function (d) { return d.value; })
+
+
+  //=====================================================================//
+
+
+  // Line chart
+
+  var height = 300; // Chart height
+
+  // The number of datapoints
+  var n = 21;
+
+  // X scale will use the index of our data
+  var xScale = d3.scaleLinear()
+    .domain([0, n - 1]) // input
+    .range([0, dimensions.gInnerWidth]); // output
+
+  // Y scale will use the randomly generate number 
+  var yScale = d3.scaleLinear()
+    .domain([0, 1]) // input 
+    .range([height, 0]); // output 
+
+  // d3's line generator
+  var line = d3.line()
+    .x(function (d, i) { return xScale(i); }) // set the x values for the line generator
+    .y(function (d) { return yScale(d.y); }) // set the y values for the line generator 
+    .curve(d3.curveMonotoneX) // apply smoothing to the line
+
+  // An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
+  var dataset = d3.range(n).map(function (d) { return { "y": d3.randomUniform(1)() } })
+
+  // Add the SVG to the page and employ #2
+  var lineChart = chart.append("g")
+    .attr("transform", "translate(" + 50 + "," + 1000 + ")");
+
+  // Call the x axis in a group tag
+  lineChart.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
+
+  // Call the y axis in a group tag
+  lineChart.append("g")
+    .attr("class", "y axis")
+    .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
+
+  // Append the path, bind the data, and call the line generator 
+  lineChart.append("path")
+    .datum(dataset) // 10. Binds data to the line 
+    .attr("class", "line") // Assign a class for styling 
+    .attr("d", line) // 11. Calls the line generator 
+    .style("fill", "none")
+    .style("stroke", "#ffab00");
+
+  // Appends a circle for each datapoint 
+  lineChart.selectAll(".dot")
+    .data(dataset)
+    .enter().append("circle") // Uses the enter().append() method
+    .attr("class", "dot") // Assign a class for styling
+    .attr("cx", function (d, i) { return xScale(i) })
+    .attr("cy", function (d) { return yScale(d.y) })
+    .attr("r", 5)
+    .style("fill", "#ffab00")
+    .style("stroke", "#fff")
+
+
+  //=====================================================================//
+
 
   // Convert to PDF
   var htmlString = d3n.chartHTML();
+
   var fileName = 'myChart.pdf';
-  var pdfOptions = { format: 'Letter', phantomPath: '/opt/node_modules/phantomjs_lambda/phantomjs_linux-x86_64' }; // 'html-pdf' is using phantomjs internally, and there is no direct npm lib availalble for it, so I manually include executable of phantomjs.
+  var pdfOptions = { format: 'Letter', phantomPath: '/opt/node_modules/phantomjs_lambda/phantomjs_linux-x86_64' }; // 'html-pdf' is using phantomjs-prebuilt internally, so we manually included executable of phantomjs into Layers. You can find executable here 'https://github.com/TylerPachal/lambda-node-phantom'.
 
   //Create the PDF file from the HTML string
   pdf.create(htmlString, pdfOptions).toBuffer(function (err, buffer) {
@@ -113,7 +239,7 @@ exports.handler = function (event, context, callback) {
     else {
       var s3 = new AWS.S3();
       var params = {
-        Bucket: '{BUCKET NAME}',
+        Bucket: 'BUCKET NAME',
         Key: fileName,
         Body: buffer
       }
